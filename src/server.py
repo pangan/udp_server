3ef8f32b2962d573cf8c1f0c142aa0707ecf0488
Amir <pangan@gmail.com>
2023-05-21 14:34:32 +0100

    added payload

--- src/server.py
+++ src/server.py
@@ -1,15 +1,29 @@
 import socket
+import json
+
+SECRET_KEY = '1234'
 
 
-def broadcast(message):
+def broadcast(command, payload):
+    data = json.dumps(
+        {
+            'secret_key': SECRET_KEY,
+            'command': command,
+            'payload': payload
+        }
+    )
+
     UDP_IP = "127.0.0.1"
     UDP_PORT = 5005
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
 
-    sock.sendto(message, (UDP_IP, UDP_PORT))
+    sock.sendto(bytes(data, encoding='utf-8'), (UDP_IP, UDP_PORT))
     print(f'message sent to {UDP_IP}:{UDP_PORT}')
 
 
 if __name__ == '__main__':
-    broadcast(b'hello')
+    broadcast(
+        command='CLOSE',
+        payload={"test":1}
+    )
